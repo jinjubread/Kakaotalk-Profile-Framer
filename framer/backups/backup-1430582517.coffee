9170d58ffa80dc530c3307e3df04b3c28bb0638c
Jinju Jang <jinjubread@gmail.com>
2015-05-08 18:12:47 +1000

    Initial Commit

---
+++ framer/backups/backup-1430582517.coffee
@@ -0,0 +1,71 @@
+# This imports all the layers for "kakaoProfile" into kakaoprofileLayers
+doc = Framer.Importer.load "imported/kakaoProfile"
+Utils.globalLayers(doc)
+
+# profileContainer = Photo + white box
+profileContainer.draggable.enabled = true
+profileContainer.draggable.horizontal = false
+originX = profileContainer.x
+originY = profileContainer.y
+bottomY = profileContainer.maxY
+
+# heroImage = Container for heroBG
+heroOriginY = heroImage.y
+heroheight = heroImage.height
+profilewrapOriginHeight = profilewrap.height
+
+# heroBG = Background Image 
+heroBG.originX = 0.5
+heroBgHeight = heroBG.height
+
+# statusLabel = "안녕!"
+statusOriginY = statusLabel.y
+
+whiteBG = new BackgroundLayer
+	backgroundColor: "white"
+
+# ------- Events
+# Up & Down: Image gets bigger, statusLabel moves, whitebox stretches
+profileContainer.on Events.DragMove, ->
+	profilewrap.height = profilewrapOriginHeight + (bottomY - 
+	profileContainer.y)	
+	statusLabel.y = Utils.modulate(profileContainer.y, [0,Screen.height], [0, 668], true)
+		
+	scrollChecker = (profileContainer.y - profilewrapOriginHeight - 162)
+
+	if scrollChecker >= 0
+		heroImage.scale = Utils.modulate(profileContainer.y, [0, Screen.height], [1, 1.2], true)
+		heroBG.scale = heroImage.scale
+	else
+		statusLabel.opacity = Utils.modulate(profileContainer.y, [0,statusOriginY], [0, 1], true)
+		statusLabel.scale = Utils.modulate(profileContainer.y, [0,statusOriginY], [0.5, 1], true)
+		closeIcon.opacity = Utils.modulate(profileContainer.y, [0,statusOriginY], [0, 1], true)
+
+# Back to the initial positions.	
+profileContainer.on Events.DragEnd, ->
+	profileContainer.animate
+		properties:
+			x: originX
+			y: originY
+		time: 0.4
+		curve: "ease"
+			
+	heroBG.animate
+		properties:
+			scale: 1 
+		time: 0.3
+		curve: "ease"
+	
+	statusLabel.animate
+		properties:
+			y: statusOriginY
+			opacity:1
+			scale: 1
+		time: 0.3
+		curve: "ease"	
+				
+	closeIcon.animate
+		properties:
+			opacity:1
+		time: 0.3
+		curve: "ease"		
---
+++ imported/kakaoProfile/layers.json.js
@@ -0,0 +1,313 @@
+window.__imported__ = window.__imported__ || {};
+window.__imported__["kakaoProfile/layers.json.js"] = [
+  {
+    "maskFrame" : null,
+    "children" : [
+      {
+        "maskFrame" : null,
+        "id" : "25073E2B-AD2F-4068-81E7-B662025591F1",
+        "visible" : true,
+        "children" : [
+
+        ],
+        "image" : {
+          "path" : "images\/Layer-statusLabel-25073E2B-AD2F-4068-81E7-B662025591F1.png",
+          "frame" : {
+            "y" : 358,
+            "x" : 318,
+            "width" : 112,
+            "height" : 100
+          }
+        },
+        "imageType" : "png",
+        "layerFrame" : {
+          "y" : 358,
+          "x" : 318,
+          "width" : 112,
+          "height" : 100
+        },
+        "name" : "statusLabel"
+      },
+      {
+        "maskFrame" : null,
+        "id" : "AB715512-4C34-4103-ACC5-72812C5436CA",
+        "visible" : true,
+        "children" : [
+
+        ],
+        "image" : {
+          "path" : "images\/Layer-closeIcon-AB715512-4C34-4103-ACC5-72812C5436CA.png",
+          "frame" : {
+            "y" : 32,
+            "x" : 27,
+            "width" : 47,
+            "height" : 47
+          }
+        },
+        "imageType" : "png",
+        "layerFrame" : {
+          "y" : 32,
+          "x" : 27,
+          "width" : 47,
+          "height" : 47
+        },
+        "name" : "closeIcon"
+      },
+      {
+        "maskFrame" : null,
+        "children" : [
+          {
+            "maskFrame" : {
+              "y" : 668,
+              "x" : 265,
+              "width" : 220,
+              "height" : 220
+            },
+            "id" : "10431B67-0422-4659-9D53-77005E58DDBC",
+            "visible" : true,
+            "children" : [
+
+            ],
+            "image" : {
+              "path" : "images\/Layer-profileImage-10431B67-0422-4659-9D53-77005E58DDBC.png",
+              "frame" : {
+                "y" : 668,
+                "x" : 265,
+                "width" : 220,
+                "height" : 220
+              }
+            },
+            "imageType" : "png",
+            "layerFrame" : {
+              "y" : 668,
+              "x" : 265,
+              "width" : 220,
+              "height" : 220
+            },
+            "name" : "profileImage"
+          },
+          {
+            "maskFrame" : null,
+            "id" : "8080F387-4DE5-4DA8-BD7B-BE8B114488E5",
+            "visible" : true,
+            "children" : [
+
+            ],
+            "image" : {
+              "path" : "images\/Layer-email-8080F387-4DE5-4DA8-BD7B-BE8B114488E5.png",
+              "frame" : {
+                "y" : 968,
+                "x" : 208,
+                "width" : 330,
+                "height" : 58
+              }
+            },
+            "imageType" : "png",
+            "layerFrame" : {
+              "y" : 968,
+              "x" : 208,
+              "width" : 330,
+              "height" : 58
+            },
+            "name" : "email"
+          },
+          {
+            "maskFrame" : null,
+            "id" : "113FD15A-59AB-4A6E-A5B1-F20FCCA84CA7",
+            "visible" : true,
+            "children" : [
+
+            ],
+            "image" : {
+              "path" : "images\/Layer-nameContainer-113FD15A-59AB-4A6E-A5B1-F20FCCA84CA7.png",
+              "frame" : {
+                "y" : 914,
+                "x" : 333,
+                "width" : 80,
+                "height" : 38
+              }
+            },
+            "imageType" : "png",
+            "layerFrame" : {
+              "y" : 914,
+              "x" : 333,
+              "width" : 80,
+              "height" : 38
+            },
+            "name" : "nameContainer"
+          },
+          {
+            "maskFrame" : null,
+            "id" : "EAA5769F-BC63-4E8F-A22E-3B6C25FE0BDB",
+            "visible" : true,
+            "children" : [
+
+            ],
+            "image" : {
+              "path" : "images\/Layer-btn3-EAA5769F-BC63-4E8F-A22E-3B6C25FE0BDB.png",
+              "frame" : {
+                "y" : 1100,
+                "x" : 490,
+                "width" : 131,
+                "height" : 130
+              }
+            },
+            "imageType" : "png",
+            "layerFrame" : {
+              "y" : 1100,
+              "x" : 490,
+              "width" : 131,
+              "height" : 130
+            },
+            "name" : "btn3"
+          },
+          {
+            "maskFrame" : null,
+            "id" : "BAF20EB4-2880-46A8-9526-0C607FCF5222",
+            "visible" : true,
+            "children" : [
+
+            ],
+            "image" : {
+              "path" : "images\/Layer-btn2-BAF20EB4-2880-46A8-9526-0C607FCF5222.png",
+              "frame" : {
+                "y" : 1100,
+                "x" : 305,
+                "width" : 130,
+                "height" : 130
+              }
+            },
+            "imageType" : "png",
+            "layerFrame" : {
+              "y" : 1100,
+              "x" : 305,
+              "width" : 130,
+              "height" : 130
+            },
+            "name" : "btn2"
+          },
+          {
+            "maskFrame" : null,
+            "id" : "16171FBD-0D6D-4EE6-9CB9-574F42BDFDB3",
+            "visible" : true,
+            "children" : [
+
+            ],
+            "image" : {
+              "path" : "images\/Layer-btn1-16171FBD-0D6D-4EE6-9CB9-574F42BDFDB3.png",
+              "frame" : {
+                "y" : 1100,
+                "x" : 128,
+                "width" : 130,
+                "height" : 130
+              }
+            },
+            "imageType" : "png",
+            "layerFrame" : {
+              "y" : 1100,
+              "x" : 128,
+              "width" : 130,
+              "height" : 130
+            },
+            "name" : "btn1"
+          },
+          {
+            "maskFrame" : null,
+            "id" : "8847E05B-4312-4CB5-AEED-F4D979F94B4A",
+            "visible" : true,
+            "children" : [
+
+            ],
+            "image" : {
+              "path" : "images\/Layer-profilewrap-8847E05B-4312-4CB5-AEED-F4D979F94B4A.png",
+              "frame" : {
+                "y" : 828,
+                "x" : 0,
+                "width" : 750,
+                "height" : 506
+              }
+            },
+            "imageType" : "png",
+            "layerFrame" : {
+              "y" : 828,
+              "x" : 0,
+              "width" : 750,
+              "height" : 506
+            },
+            "name" : "profilewrap"
+          }
+        ],
+        "id" : "78B29E07-702E-4797-8C08-2E7A8FA3A0D0",
+        "visible" : true,
+        "imageType" : "png",
+        "layerFrame" : {
+          "y" : 668,
+          "x" : 0,
+          "width" : 750,
+          "height" : 666
+        },
+        "name" : "profileContainer"
+      },
+      {
+        "maskFrame" : null,
+        "id" : "CFA5AAE2-844C-44B6-B88A-0D69C70E12D6",
+        "visible" : true,
+        "children" : [
+
+        ],
+        "image" : {
+          "path" : "images\/Layer-heroBG-CFA5AAE2-844C-44B6-B88A-0D69C70E12D6.png",
+          "frame" : {
+            "y" : 0,
+            "x" : 0,
+            "width" : 750,
+            "height" : 934
+          }
+        },
+        "imageType" : "png",
+        "layerFrame" : {
+          "y" : 0,
+          "x" : 0,
+          "width" : 750,
+          "height" : 934
+        },
+        "name" : "heroBG"
+      },
+      {
+        "maskFrame" : null,
+        "id" : "539C2705-22EE-4EDF-AD56-3CCF3FE4B23B",
+        "visible" : true,
+        "children" : [
+
+        ],
+        "image" : {
+          "path" : "images\/Layer-heroImage-539C2705-22EE-4EDF-AD56-3CCF3FE4B23B.png",
+          "frame" : {
+            "y" : 0,
+            "x" : 0,
+            "width" : 750,
+            "height" : 828
+          }
+        },
+        "imageType" : "png",
+        "layerFrame" : {
+          "y" : 0,
+          "x" : 0,
+          "width" : 750,
+          "height" : 828
+        },
+        "name" : "heroImage"
+      }
+    ],
+    "id" : "C1B4FA09-F5B6-40D9-8AB8-6FF70C6E2CE0",
+    "visible" : true,
+    "imageType" : "png",
+    "layerFrame" : {
+      "y" : 0,
+      "x" : 0,
+      "width" : 750,
+      "height" : 1334
+    },
+    "name" : "Profile"
+  }
+]
